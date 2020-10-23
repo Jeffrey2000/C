9e4980cb081a5633e22ee0b1fd94ffe7484b57d5
Jeffrey2000 <jeffreykola15@gmail.com>
2020-10-23 17:32:53 +0100

    second way using fgets

--- prac2.c
+++ prac2.c
@@ -1,51 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXLINE 100
+#define AGE_LEN 2
 //files are identified by using their file pointer
 // this is obtained when the file is opened
 
 
 
 
-int fgetline(FILE *fp, char line[], int max){
-    //compound conditionals MUST have surrounding brackets
-    if( (fp = fopen("text.txt", "r+")) != NULL){
-        int nch=0 , c;
-        max--;
-        while((c = getc(fp)) != EOF){
-                if(c == '\n')
-                    break;
-                if(nch < max){ //ensure that the
-                    line[nch] = c;
-                    nch++;
-                }
-        }
 
-
-        fclose(fp);
-        if(c == EOF && nch == 0)
-            return EOF;
-
-
-        line[nch] = '\0';
-        return nch;
-    }else{
-        printf("Unable to open file\n");
-    }
-    return -1;
-}
 
 
 
 int main(){
     FILE *fp;
-    char line[MAXLINE];
-    // must initialize pointer front of array
-    int *ptr = line;
+    char name[MAXLINE];
+    char height[MAXLINE];
+    int age;
+    if((fp=fopen("text.txt", "r") != NULL)){
+        fp = fopen("text.txt", "r");
 
-    fgetline(fp,line,MAXLINE);
+        fgets(name, MAXLINE, fp);
+        fgets(age, MAXLINE, fp);
+        fgets(height, MAXLINE, fp);
 
-    printf(" %s", ptr);
-    return 0;
+        *height = (double)(*height);
+        printf("name=%s, age=%s, height=%s", name, age, height);
+        fclose(fp);
+        return 0;
+    }
+
+    printf("Could not open file\n");
+    return 1;
 }
 
